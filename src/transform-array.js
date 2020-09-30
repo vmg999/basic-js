1ee2baf13243e09403c21ae530ec099134156092
vmg999 <vmg999@gmail.com>
2020-09-30 18:49:35 +0300

    feat: implement transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+	if(Array.isArray(arr)){
+		res=[];
+		for(let i=0; i<arr.length; i++){
+
+				if(arr[i]=='--discard-next'){
+					if(i<arr.length-1){
+						i+=1;	
+					}
+				}else if(arr[i]=='--discard-prev'){
+					if(i>0 && arr[i-2]!='--discard-next'){
+					res.pop();
+					}
+				}else if(arr[i]=='--double-next'){
+					if(i<arr.length-1){
+					res.push(arr[i+1]);
+					}
+				}else if(arr[i]=='--double-prev'){
+					if(i>0 && arr[i-2]!='--discard-next'){
+					res.push(arr[i-1]);
+					}
+				}else{
+					res.push(arr[i]);
+				}
+			}
+		return res;
+	}else{
+		throw 'Error';
+	}
 };
